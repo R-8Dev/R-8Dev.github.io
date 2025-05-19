e80e12d4c32673c7773f825a06d6516adf9f11fb
R-8Dev <giogogitidz@gmail.com>
2025-05-19 16:05:53 +0400

    Update firebase-init.js

--- docs/firebase-init.js
+++ docs/firebase-init.js
@@ -1,5 +1,7 @@
+// Declare firebaseConfig only once globally
 if (typeof firebaseConfig === 'undefined') {
-  const firebaseConfig = {
+  window.firebaseConfig = {
+    
   apiKey: "AIzaSyC0a5wUtspLGBPpJ6XS6Axg-8XsgiiZbZI",
   authDomain: "r-8-test.firebaseapp.com",
   projectId: "r-8-test",
@@ -7,9 +9,8 @@ if (typeof firebaseConfig === 'undefined') {
   messagingSenderId: "289094248821",
   appId: "1:289094248821:web:aabdd412a0e5a6e5730b3c",
   measurementId: "G-62PF9Z5RNY"
-};
+  };
 }
- 
 
 // Initialize Firebase only if not already initialized
 if (!firebase.apps.length) {
@@ -20,6 +21,7 @@ if (!firebase.apps.length) {
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-// Make auth and db available globally so other scripts can use them
+// Make auth and db globally accessible
 window.auth = auth;
 window.db = db;
+
